ea43a8956c5a5305000c5a7c5efbe312f1782f68
Hiroshi Himeno <hiroshi.himeno@sonybn.co.jp>
2021-07-01 00:24:52 +0900

    resume ver2.2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" width="25%">
-    <HelloWorld msg="Hello Vue in CodeSandbox! ver2.1"/>
+    <HelloWorld msg="Hello Vue in CodeSandbox! ver2.2"/>
   </div>
 </template>
 
