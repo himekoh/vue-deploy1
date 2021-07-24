624f49ab81b06d78006b00dcbc7e7c567ca62385
rossy <h2tafemi@gmail.com>
2021-07-24 10:22:59 +0900

    ver.2.4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" width="25%">
-    <HelloWorld msg="Hello Vue in CodeSandbox! ver2.3"/>
+    <HelloWorld msg="Hello Vue in CodeSandbox! ver2.4"/>
   </div>
 </template>
 
